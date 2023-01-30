85c59b968174254bac22845925a6bd0b173fda4b
Xisca04 <xiscacerdavicens@gmail.com>
2023-01-30 10:46:34 +0100

    powerup

--- Assets/Scrpts/PlayerController.cs
+++ Assets/Scrpts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour
     // Tener de referencia al Focal Point para mejorara la direccion del player segun la camara y su movimiento
     private GameObject focalPoint;
 
+    public bool hasPowerup;
+    private float powerupForce = 15f;
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -21,5 +24,31 @@ public class PlayerController : MonoBehaviour
     {
         forwardInput = Input.GetAxis("Vertical");
         _rigidbody.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            StartCoroutine(PowerupCountDown());
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position).normalized;
+            enemyRigidbody.AddForce(awayFromPlayer * powerupForce, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator PowerupCountDown()
+    {
+        yield return new WaitForSeconds(6);
+        hasPowerup = false;
     }
 }
