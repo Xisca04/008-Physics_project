8358096d6ae38af6b9a2a71f9525d3e77c45cb33
Xisca04 <xiscacerdavicens@gmail.com>
2023-02-06 09:47:24 +0100

    Bucles

--- Assets/Scrpts/Bucles.cs
+++ Assets/Scrpts/Bucles.cs
@@ -10,10 +10,16 @@ public class Bucles : MonoBehaviour
     public Vector3[] positions;
     public GameObject prefab;
 
+    // Elementos para los bucles WHILE
+    private int i = 1;
+    public int value = 9;
+    
     void Start()
     {
+        // Bucles for
+        
         // Muestra los numeros del 1 al 10
-        /*for(int i = 1; i <= 10; i++)
+        for(int i = 1; i <= 10; i++)
         {
             Debug.Log(i);
         }
@@ -37,11 +43,42 @@ public class Bucles : MonoBehaviour
         {
             Debug.Log(names[i]);
         }
-        */
-        // Instanciar posiciones
+        
+        // Instanciar elementos en diferentes posiciones
         for(int i = 0; i < positions.Length; i++)
         {
             Instantiate(prefab, positions[i], Quaternion.identity);
+        }
+        
+        // Bucles foreach
+
+        // Recorrer array de strings
+        foreach(string name in names)
+        {
+            Debug.Log(name);
+        }
+
+        // Instanciar elementos
+        foreach(Vector3 p in positions)
+        {
+            Instantiate(prefab, p, Quaternion.identity);
+        }
+        
+
+        // Bucles while
+
+        // Mostrar numeros del 1 al 10
+        while(i <= 10)
+        {
+            Debug.Log(i);
+            i++;
+        }
+        
+        // Tabla del 9
+        while(i <= 10)
+        {
+            Debug.Log($"{value} x {i} = {value * i}");
+            i++;
         }
     }
 
